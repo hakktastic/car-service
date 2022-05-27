6df4eb87371e18e194b0dfceaba1b1a16424ba3b
hakktastic <hakktastic@gmail.com>
2022-05-27 08:17:16 +0200

    initial commit

---
+++ src/main/java/nl/hakktastic/leaseacarapi/CarServiceApplication.java
@@ -0,0 +1,20 @@
+package nl.hakktastic.leaseacarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Car service application.
+ */
+@SpringBootApplication
+public class CarServiceApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceApplication.class, args);
+    }
+}
