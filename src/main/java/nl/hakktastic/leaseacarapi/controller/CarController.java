4a0fb1fc1cea0612219d7102d58bf768be1aebba
hakktastic <hakktastic@gmail.com>
2022-07-18 20:23:26 +0200

    HAK-48 organize imports
    -

--- src/main/java/nl/hakktastic/leaseacarapi/controller/CarController.java
+++ src/main/java/nl/hakktastic/leaseacarapi/controller/CarController.java
@@ -3,8 +3,6 @@ package nl.hakktastic.leaseacarapi.controller;
 import lombok.extern.slf4j.Slf4j;
 import nl.hakktastic.leaseacarapi.entity.Car;
 import nl.hakktastic.leaseacarapi.service.CarService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
