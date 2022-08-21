5a164604a4db12bab8f516b2a7e95df335b434ab
hakktastic <hakktastic@gmail.com>
2022-08-21 11:19:40 +0200

    HAK-42 add hibernate validations
    -

--- src/main/java/nl/hakktastic/leaseacarapi/entity/Car.java
+++ src/main/java/nl/hakktastic/leaseacarapi/entity/Car.java
@@ -1,15 +1,23 @@
 package nl.hakktastic.leaseacarapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /** JPA Car Entity. */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Car {
 
@@ -17,46 +25,31 @@ public class Car {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
+  @NotNull
+  @Length(min = 2, max = 50)
   private String make;
+
+  @NotNull
+  @Length(min = 2, max = 50)
   private String model;
+
+  @NotNull
+  @Length(min = 1, max = 50)
   private String version;
 
-  @Size(min = 2, message = "A car should have at least two doors")
+  @NotNull
+  @Size(min = 2)
   private int numberOfDoors;
 
+  @NotNull
+  @Length(min = 1)
   private double grossPrice;
+
+  @NotNull
+  @Length(min = 1)
   private double nettPrice;
+
+  @NotNull
+  @Length(min = 1)
   private int hp;
-
-  /** Default Constructor. */
-  public Car() {}
-
-  /**
-   * Constructor.
-   *
-   * @param make Make of Car
-   * @param model Model of Car
-   * @param version Version of Car
-   * @param numberOfDoors Number of Doors of Car
-   * @param grossPrice Gross Price of Car
-   * @param nettPrice Nett Price of Car
-   * @param hp HP of Car
-   */
-  public Car(
-      String make,
-      String model,
-      String version,
-      int numberOfDoors,
-      double grossPrice,
-      double nettPrice,
-      int hp) {
-    super();
-    this.make = make;
-    this.model = model;
-    this.version = version;
-    this.numberOfDoors = numberOfDoors;
-    this.grossPrice = grossPrice;
-    this.nettPrice = nettPrice;
-    this.hp = hp;
-  }
 }
