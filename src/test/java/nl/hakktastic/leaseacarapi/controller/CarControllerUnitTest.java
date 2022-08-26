fb45fa8304973568e70bc5be0da947b28e31caab
hakktastic <hakktastic@gmail.com>
2022-08-26 13:51:24 +0200

    HAK-42 add tests
    -

--- src/test/java/nl/hakktastic/leaseacarapi/controller/CarControllerUnitTest.java
+++ src/test/java/nl/hakktastic/leaseacarapi/controller/CarControllerUnitTest.java
@@ -1,6 +1,7 @@
 package nl.hakktastic.leaseacarapi.controller;
 
 import nl.hakktastic.leaseacarapi.service.CarService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static nl.hakktastic.leaseacarapi.testdata.CarTestData.*;
@@ -133,23 +136,93 @@ public class CarControllerUnitTest {
   }
 
   @Test
-  public void givenInvalidGrossPrice_whenCreateCar_thenReturnNotFound() {}
+  public void givenInvalidGrossPrice_whenCreateCar_thenReturnNotFound() {
+
+    when(carService.createCar(CAR_OBJECT_INVALID_GROSS_PRICE)).thenReturn(Optional.empty());
+
+    var responseEntity = carController.createCar(CAR_OBJECT_INVALID_GROSS_PRICE);
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    assertThat(responseEntity.getBody()).isNull();
+  }
 
   @Test
-  public void givenInvalidNettPrice_whenCreateCar_thenReturnNotFound() {}
+  public void givenInvalidNettPrice_whenCreateCar_thenReturnNotFound() {
+
+    when(carService.createCar(CAR_OBJECT_INVALID_NETT_PRICE)).thenReturn(Optional.empty());
+
+    var responseEntity = carController.createCar(CAR_OBJECT_INVALID_NETT_PRICE);
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    assertThat(responseEntity.getBody()).isNull();
+  }
 
   @Test
-  public void givenValidId_whenDeleteCar_thenReturnOK() {}
+  public void givenValidId_whenDeleteCar_thenReturnOK() {
+
+    when(carService.deleteCar(CAR_ID_VALID_2891_FORD)).thenReturn(Boolean.TRUE);
+
+    var responseEntity = carController.deleteCar(CAR_ID_VALID_2891_FORD);
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(responseEntity.getBody()).isEqualTo(StringUtils.EMPTY);
+  }
 
   @Test
-  public void givenInvalidId_whenDeleteCar_thenReturnNotFound() {}
+  public void givenInvalidId_whenDeleteCar_thenReturnNotFound() {
+
+    when(carService.deleteCar(CAR_ID_INVALID_0)).thenReturn(Boolean.FALSE);
+
+    var responseEntity = carController.deleteCar(CAR_ID_INVALID_0);
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    assertThat(responseEntity.getBody()).isEqualTo(StringUtils.EMPTY);
+  }
 
   @Test
-  public void givenValidId_whenGetCar_thenReturnOK() {}
+  public void givenValidId_whenGetCar_thenReturnOK() {
+
+    when(carService.getSingleCar(CAR_ID_VALID_11329_VOLVO))
+        .thenReturn(Optional.ofNullable(CAR_OBJECT_VALID_ALL_ARGS_VOLVO));
+
+    var responseEntiy = carController.getCar(CAR_ID_VALID_11329_VOLVO);
+
+    assertThat(responseEntiy.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(responseEntiy.getBody()).isEqualTo(CAR_OBJECT_VALID_ALL_ARGS_VOLVO);
+  }
 
   @Test
-  public void givenInvalidId_whenGetCar_thenReturnNotFound() {}
+  public void givenInvalidId_whenGetCar_thenReturnNotFound() {
+
+    when(carService.getSingleCar(CAR_ID_INVALID_0)).thenReturn(Optional.empty());
+
+    var responseEntiy = carController.getCar(CAR_ID_INVALID_0);
+
+    assertThat(responseEntiy.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    assertThat(responseEntiy.getBody()).isNull();
+  }
 
   @Test
-  public void givenCarsExistInRepository_whenGetCars_thenReturnOK() {}
+  public void givenTwoCarsExistInRepository_whenGetCars_thenReturnOK() {
+
+    when(carService.getAllCars())
+        .thenReturn(List.of(CAR_OBJECT_VALID_ALL_ARGS_VOLVO, CAR_OBJECT_VALID_ALL_ARGS_LAND_ROVER));
+
+    var responseEntity = carController.getCars();
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(responseEntity.getBody())
+        .containsExactly(CAR_OBJECT_VALID_ALL_ARGS_VOLVO, CAR_OBJECT_VALID_ALL_ARGS_LAND_ROVER);
+  }
+
+  @Test
+  public void givenNoCarsExistInRepository_whenGetCars_thenReturnNotFound() {
+
+    when(carService.getAllCars()).thenReturn(Collections.emptyList());
+
+    var responseEntity = carController.getCars();
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    assertThat(responseEntity.getBody()).isEqualTo(Collections.emptyList());
+  }
 }
